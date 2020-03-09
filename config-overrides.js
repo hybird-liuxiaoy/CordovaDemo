e7f6c5849c1188b64d818262ab79f8c5fc02309d
liuxy <938651469@qq.com>
2020-03-09 15:33:04 +0800

    update eslint

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,16 @@
 const path = require('path');
-const {override, addWebpackAlias} = require('customize-cra');
+const { override, addWebpackAlias } = require('customize-cra');
 
 module.exports = override(
-    addWebpackAlias({
-        ['erc']: path.resolve(__dirname, 'src')
-    })
+  addWebpackAlias({
+    erc: path.resolve(__dirname, 'src'),
+  })
 );
 
 exports = module.exports;
 
+// eslint-disable-next-line no-shadow
 exports.paths = function paths(paths, env) {
-    paths.appBuild = path.resolve(__dirname, "www");
-    return paths;
+  paths.appBuild = path.resolve(__dirname, 'www');
+  return paths;
 };
